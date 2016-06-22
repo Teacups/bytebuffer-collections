d1425e3a75bc91b52fb8ef1a99b1065c01b85fc0
ldm-thomas.peyric <thomas.peyric@ldmobile.net>
2016-06-22 11:57:06 +0200

    patch RadiusBound.java 2.4.0

--- src/main/java/com/metamx/collections/spatial/search/RadiusBound.java
+++ src/main/java/com/metamx/collections/spatial/search/RadiusBound.java
@@ -26,84 +26,101 @@ import com.metamx.collections.spatial.ImmutablePoint;
  */
 public class RadiusBound extends RectangularBound
 {
-  private static float[] getMinCoords(float[] coords, float radius)
-  {
-    float[] retVal = new float[coords.length];
-    for (int i = 0; i < coords.length; i++) {
-      retVal[i] = coords[i] - radius;
-    }
-    return retVal;
-  }
+	private static float[] getMinCoords(float[] coords, float radius)
+	{
+		float[] retVal = new float[coords.length];
+		for (int i = 0; i < coords.length; i++) {
+			retVal[i] = coords[i] - radius;
+		}
+		return retVal;
+	}
 
-  private static float[] getMaxCoords(float[] coords, float radius)
-  {
-    float[] retVal = new float[coords.length];
-    for (int i = 0; i < coords.length; i++) {
-      retVal[i] = coords[i] + radius;
-    }
-    return retVal;
-  }
+	private static float[] getMaxCoords(float[] coords, float radius)
+	{
+		float[] retVal = new float[coords.length];
+		for (int i = 0; i < coords.length; i++) {
+			retVal[i] = coords[i] + radius;
+		}
+		return retVal;
+	}
 
-  private final float[] coords;
-  private final float radius;
+	private final float[]	coords;
+	private final float		radius;
 
-  @JsonCreator
-  public RadiusBound(
-      @JsonProperty("coords") float[] coords,
-      @JsonProperty("radius") float radius,
-      @JsonProperty("limit") int limit
-  )
-  {
-    super(getMinCoords(coords, radius), getMaxCoords(coords, radius), limit);
+	@JsonCreator
+	public RadiusBound(
+		@JsonProperty("coords") float[] coords,
+		@JsonProperty("radius") float radius,
+		@JsonProperty("limit") int limit)
+	{
+		super(getMinCoords(coords, radius), getMaxCoords(coords, radius), limit);
 
-    this.coords = coords;
-    this.radius = radius;
-  }
+		this.coords = coords;
+		this.radius = radius;
+	}
 
-  public RadiusBound(
-      float[] coords,
-      float radius
-  )
-  {
-    this(coords, radius, 0);
-  }
+	public RadiusBound(
+		float[] coords,
+		float radius)
+	{
+		this(coords, radius, 0);
+	}
 
-  @JsonProperty
-  public float[] getCoords()
-  {
-    return coords;
-  }
+	@JsonProperty
+	public float[] getCoords()
+	{
+		return coords;
+	}
 
-  @JsonProperty
-  public float getRadius()
-  {
-    return radius;
-  }
+	@JsonProperty
+	public float getRadius()
+	{
+		return radius;
+	}
 
-  @Override
-  public boolean contains(float[] otherCoords)
-  {
-    double total = 0.0;
-    for (int i = 0; i < coords.length; i++) {
-      total += Math.pow(otherCoords[i] - coords[i], 2);
-    }
+	@Override
+	public boolean contains(float[] otherCoords)
+	//	{
+	//		double total = 0.0;
+	//		for (int i = 0; i < coords.length; i++) {
+	//			total += Math.pow(otherCoords[i] - coords[i], 2);
+	//		}
+	//
+	//		return (total <= Math.pow(radius, 2));
+	//	}
+	{
+		//    if (super.contains(otherCoords)) {
+		if (coords.length == 2) {
+			double toRadian = Math.PI / 180;
+			// lat=[0] lon=[1]
+			double x = (otherCoords[1] - coords[1]) * toRadian * Math.cos((coords[0] + otherCoords[0]) * toRadian / 2);
+			double y = (otherCoords[0] - coords[0]) * toRadian;
+			double d = Math.sqrt(x * x + y * y) * 6371000.0f;
+			return (d <= radius);
+		} else {
+			double total = 0.0;
+			for (int i = 0; i < coords.length; i++) {
+				total += Math.pow(otherCoords[i] - coords[i], 2);
+			}
+			return (total <= Math.pow(radius, 2));
+		}
+		//  }
+		//  return false;
+	}
 
-    return (total <= Math.pow(radius, 2));
-  }
-
-  @Override
-  public Iterable<ImmutablePoint> filter(Iterable<ImmutablePoint> points)
-  {
-    return Iterables.filter(
-        points,
-        new Predicate<ImmutablePoint>()
-        {
-          @Override
-          public boolean apply(ImmutablePoint point)
-          {
-            return contains(point.getCoords());
-          }
-        }
-    );
-  }
+	@Override
+	public Iterable<ImmutablePoint> filter(Iterable<ImmutablePoint> points)
+	{
+		return Iterables.filter(
+			points,
+			new Predicate<ImmutablePoint>()
+			{
+				@Override
+				public boolean apply(ImmutablePoint point)
+				{
+					return contains(point.getCoords());
+				}
+			}
+			);
+	}
 }
